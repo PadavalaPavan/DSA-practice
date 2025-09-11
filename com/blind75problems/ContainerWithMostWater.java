b0921aac1e9e09a87878adef2174f043ece6837b
pavankumar.padavala <pavankumar.padavala@traxtech.com>
2025-09-11 14:16:07 +0530

    container with most water problem

---
+++ com/blind75problems/ContainerWithMostWater.java
@@ -0,0 +1,31 @@
+package com.blind75problems;
+
+public class ContainerWithMostWater {
+    public static void main(String[] args) {
+        System.out.println(maxArea(new int[]{1, 8, 6, 2, 5, 4, 8, 3, 7}));
+    }
+
+    public static int maxArea(int[] height) {
+
+        int left = 0, right = height.length - 1;
+        int maxArea = 0;
+
+        while (left < right) {
+            int width = right - left;
+            int h = Math.min(height[left], height[right]);
+            int area = width * h;
+
+            if (maxArea < area) {
+                maxArea = area;
+            }
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        return maxArea;
+
+    }
+}
